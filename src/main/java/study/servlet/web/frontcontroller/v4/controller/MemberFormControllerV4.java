e1aef9ae025ed8625377e7bf5941d92c2513e7d1
who-hoo <who.ho3ov@gmail.com>
2022-03-10 03:48:54 +0900

    :sparkles: 단순하고 실용적인 컨트롤러(v4)
    기존 구조에서 모델을 파라미터로 넘기고, 뷰의 논리 이름을 반환하도록 변경 -> 항상 ModelView 객체를 생성하고 반환하지 않아도 되어 실용적

---
+++ src/main/java/study/servlet/web/frontcontroller/v4/controller/MemberFormControllerV4.java
@@ -0,0 +1,12 @@
+package study.servlet.web.frontcontroller.v4.controller;
+
+import java.util.Map;
+import study.servlet.web.frontcontroller.v4.ControllerV4;
+
+public class MemberFormControllerV4 implements ControllerV4 {
+
+    @Override
+    public String process(Map<String, String> paramMap, Map<String, Object> model) {
+        return "new-form";
+    }
+}
